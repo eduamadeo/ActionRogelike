94cf23736cc8ac0298bebbb4771e9744ad067fbf
Eduardo Nardi <eduardonardi@mpg.io>
2022-09-08 17:38:20 +0100

    Add explosive barrel

---
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComp");
+	StaticMeshComp->SetSimulatePhysics(true);
+	StaticMeshComp->SetCollisionProfileName("PhysicsActor");
+	RootComponent = StaticMeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>("RadialForceComp");
+	RadialForceComp->SetupAttachment(StaticMeshComp);
+
+	RadialForceComp->SetAutoActivate(false);
+
+	RadialForceComp->Radius = 750.0f;
+	RadialForceComp->ImpulseStrength = 2500.0f;
+	RadialForceComp->bImpulseVelChange = true;
+
+	RadialForceComp->AddCollisionChannelToAffect(ECC_WorldDynamic);
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASExplosiveBarrel::PostInitializeComponents()
+{
+	// Don't forget to call parent function
+	Super::PostInitializeComponents();
+
+	StaticMeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnCompHit);
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASExplosiveBarrel::OnCompHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComp->FireImpulse();
+}
+
